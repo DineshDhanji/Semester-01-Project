7515cb123f69f03bccb69d6a277da5d069cbb05c
Dinesh Dhanji <56204876+DineshDhanji@users.noreply.github.com>
2022-03-12 17:30:05 +0500

    Delete Copy directory

--- Copy/review.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-void main (){
-	int review;
-	printf("Thank you for Shopping\n");
-	printf("Plz give review :");
-	printf("1: Excellent 2:Good 3:Satisfied 4:Bad 5:Very Bad\n");
-	scanf("%d",&review);
-}
